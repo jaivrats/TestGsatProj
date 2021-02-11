5179d4138667f9ca3b37a794d4673cd2913e5230
Sathish G <57170450+gsatkani@users.noreply.github.com>
2021-02-12 01:33:18 +0530

    Update App.java

--- src/main/java/com/gsat/test/App.java
+++ src/main/java/com/gsat/test/App.java
@@ -1,13 +1,22 @@
 package com.gsat.test;
 
+import java.util.logging.Level; 
+import java.util.logging.Logger; 
+import java.util.logging.*; 
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
+    
+    private final static Logger LOGGER =  
+                Logger.getLogger(Logger.GLOBAL_LOGGER_NAME); 
     public static void main( String[] args )
     {
+        
         System.out.println( "Hello World!" );
+        LOGGER.log(Level.INFO, "My first Log Message"); 
     }
 }
